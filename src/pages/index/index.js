59fffe3461d365eedf32067c5b26c9aa3fd13385
bowl23 <1467059904@qq.com>
2023-12-26 13:29:43 +0800

    老虎机下拉全不选功能

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -8,13 +8,12 @@ function Index() {
     const [innerFlag, setInnerFlag] = useState(false);
     const [dropDown, setDropDown] = useState(false);
     const [result, setResult] = useState(false);
-
+    // 下拉菜单
     const listDown = () => {
         setDropDown(!dropDown)
     }
 
-
-
+    // 老虎机动画
     const animation = (e) => {
         console.log(e.pageX, e.pageY);
         if (e.pageX >= 360 && e.pageX <= 420 && e.pageY >= 300 && e.pageY <= 500) {
@@ -29,10 +28,24 @@ function Index() {
         }
     }
 
+    // 弹窗退出
     const resultDisappear = (e) => {
         console.log(e.pageX, e.pageY);
         if (e.pageY < 200 || e.pageY > 650) {
             setResult(false)
+        }
+    }
+
+
+    const allNotSelect = () => {
+        const allnot = document.querySelector('#allnot');
+        // console.log(allnot.checked);
+        const tag = document.querySelectorAll('.tag');
+        // console.log(select);
+        if (allnot.checked) {
+            tag.forEach((item) => {
+                item.checked = false;
+            })
         }
     }
 
@@ -48,50 +61,61 @@ function Index() {
                     <div className="position">
                         <div className="title">位置</div>
                         <div className="checkList">
-                            <input type="checkbox" name="west" id="west" />
-                            <label htmlFor="">西边</label>
-                            <input type="checkbox" name="middle" id="middle" />
-                            <label htmlFor="">中间</label>
-                            <input type="checkbox" name="east" id="east" />
-                            <label htmlFor="">东边</label>
+                            <label htmlFor='west'></label>
+                            <input type="checkbox" name="west" id="west" className='tag' />
+                            <span>西边</span>
+                            <label htmlFor='middle'></label>
+                            <input type="checkbox" name="middle" id="middle" className='tag' />
+                            <span>中间</span>
+                            <label htmlFor='east'></label>
+                            <input type="checkbox" name="east" id="east" className='tag' />
+                            <span>东边</span>
                         </div>
                     </div>
                     <div className="seller">
                         <div className="title">商家</div>
                         <div className="checkList">
-                            <input type="checkbox" name="inner" id="inner" />
-                            <label htmlFor="">校内</label>
-                            <input type="checkbox" name="outer" id="outer" />
-                            <label htmlFor="">校外</label>
+                            <label htmlFor='inner'></label>
+                            <input type="checkbox" name="inner" id="inner" className='tag' />
+                            <span>校内</span>
+                            <label htmlFor='outer'></label>
+                            <input type="checkbox" name="outer" id="outer" className='tag' />
+                            <span>校外</span>
                         </div>
                     </div>
                     <div className="other">
                         <div className="title">其他</div>
                         <div className="checkList">
-                            <input type="checkbox" name="" id="" />
-                            <label htmlFor="">小吃快餐</label>
-                            <input type="checkbox" name="" id="" />
-                            <label htmlFor="">异域料理</label>
+                            <label htmlFor='sanck'></label>
+                            <input type="checkbox" name="snack" id="snack" className='tag' />
+                            <span>小吃快餐</span>
+                            <label htmlFor='forign'></label>
+                            <input type="checkbox" name="forign" id="forign" className='tag' />
+                            <span>异域料理</span>
                             <br></br>
-                            <input type="checkbox" name="" id="" />
-                            <label htmlFor="">多人聚餐</label>
-                            <input type="checkbox" name="" id="" />
-                            <label htmlFor="">地域特色</label>
+                            <label htmlFor='many'></label>
+                            <input type="checkbox" name="many" id="many" className='tag' />
+                            <span>多人聚餐</span>
+                            <label htmlFor='section'></label>
+                            <input type="checkbox" name="section" id="section" className='tag' />
+                            <span>地域特色</span>
                         </div>
                     </div>
                     <div className="allNot">
-                        <input type="checkbox" name="" id="" />
-                        <label htmlFor="">全不选</label>
+                        <input type="checkbox" name="allnot" id="allnot" onClick={(e) => allNotSelect(e)} />
+                        <span>全不选</span>
+                        <label htmlFor='allnot'></label>
                     </div>
                 </div>}
             {/* 老虎机主体 */}
-            <div className={`machine ${outterFlag ? 'gif' : 'img'}`} gif onClick={(e) => animation(e)}>
+            <div className={`machine ${outterFlag ? 'gif' : 'img'}`} gif onClick={animation}>
                 <div className={`center ${innerFlag ? 'centerGif' : 'centerStart'}`}></div>
             </div>
             {/* 弹窗 */}
             {result && <div className='resultContainer' >
                 <div className='result'></div>
             </div>}
+            {/* 右下角的猫猫 */}
             <div className='cat'></div>
         </div>
     );
